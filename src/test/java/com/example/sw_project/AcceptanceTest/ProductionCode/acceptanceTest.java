4db50b96f311be6763fcd05a768ae04641f68313
safaa <safaa.nouri11@gmail.com>
2025-05-19 19:58:20 +0200

    Safaa Nouri & Lana Albustami

--- src/test/java/com/example/sw_project/AcceptanceTest/ProductionCode/acceptanceTest.java
+++ src/test/java/com/example/sw_project/AcceptanceTest/ProductionCode/acceptanceTest.java
@@ -1,13 +1,13 @@
 package com.example.sw_project.AcceptanceTest.ProductionCode;
+
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:\\Users\\safaa\\IdeaProjects\\sw_project\\cases", // مسار ملفات .feature
-        glue = "com.example.sw_project"     // الباكيج الذي يحتوي خطوات الاختبار
-
+        features = "src/main/resources/cases",        // استخدم مسار نسبي فقط
+        glue = "com.example.sw_project.AcceptanceTest.ProductionCode" // ضع الباكيج الصحيح لخطوات Given/When/Then
 )
 public class acceptanceTest {
 }
